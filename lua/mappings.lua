5b172460b0810a8080f7c84d0416499ce5e214e5
dcdleafy <allgaier.alif@gmail.com>
2025-01-28 18:53:21 +0000

    added hurl support and some mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,10 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local treeapi = require "nvim-tree.api"
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<leader><leader>", treeapi.tree.toggle)
+map("n", "<leader>?", vim.lsp.diagnostic.get_line_diagnostics)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,6 +32,7 @@ return {
         "go",
         "gomod",
         "gosum",
+        "hurl",
       },
     },
   },
